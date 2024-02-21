c83edc286ad205c7f8846610c4efc2cf20c22d85
petyamaiko <petyamaiko@gmail.com>
2024-02-21 23:17:54 +0200

    config

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ if (process.env.NODE_ENV === 'production') {
 const app = express()
 const port = 3000
 
+app.use(ViteExpress.static())
 app.get('/api', async (req, res) => {
   const host = 'https://www.googleapis.com/youtube/v3/playlistItems'
   const playlistId = 'PLRUeMuoAjPeAEAPYC6wOWTkto-fXC5GRh'
