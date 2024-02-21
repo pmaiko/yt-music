55a0fc91f131aa64f34a3e5b7e3f1cf64a44dc34
petyamaiko <petyamaiko@gmail.com>
2024-02-22 00:04:37 +0200

    config

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve, dirname } from 'path'
-import { fileURLToPath } from 'node:url'
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue()
-  ],
-  publicDir: resolve(__dirname, 'public'),
-  build: {
-    outDir: resolve(__dirname, 'dist')
-  }
+  ]
 })
