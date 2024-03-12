82347e3dac0177ab5c5f2be1d0d0510be7c1e2e5
petyamaiko <petyamaiko@gmail.com>
2024-03-12 13:49:38 +0200

    refactor!

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const port = 3000
 if (process.env.NODE_ENV === 'production') {
   ViteExpress.config({ mode: process.env.NODE_ENV })
   app.use(express.static(resolve(__dirname, '../build')))
-  app.use(express.static(resolve(__dirname, '/vercel/output')))
+  app.use(express.static(resolve(__dirname, './vercel/output')))
   app.use(express.static(resolve(__dirname, '../static')))
   app.use(express.static(resolve(__dirname, '/tmp')))
 } else {
