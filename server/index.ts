e4760f88e27641fd734c3d18d49f69ed7ce14070
petyamaiko <petyamaiko@gmail.com>
2025-08-31 00:41:04 +0300

    update tsconfig and server: switch to CommonJS module, adjust directory includes, and refine static file handling

--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,29 @@
 import 'dotenv/config'
+import path from 'path'
 import ip from 'ip'
-import express from 'express'
+import express, { type Request, type Response } from 'express'
 import ViteExpress from 'vite-express'
-import { resolve } from 'path'
 import apiRouter from './router'
 import tgBot from './modules/tg-bot/index'
 
 const app = express()
 const port = 3001
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(resolve(__dirname, '../build')))
-  app.use(express.static(resolve(__dirname, '../static')))
-}
-
 app.use('/api', apiRouter)
 tgBot(app)
+
+if (process.env.NODE_ENV !== 'production') {
+  app.use(express.static(path.resolve(__dirname, '../build')))
+  app.use(express.static(path.resolve(__dirname, '../static')))
+
+  app.get('*', (_req: Request, res: Response) => {
+    res.sendFile(path.resolve(__dirname, '../build/index.html'))
+  })
+}
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
 
 ViteExpress.listen(app, port, () => {
   console.log(`App listening on port http://localhost:${port}`)
