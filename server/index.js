1af6ece716f418d6e8867c25a08bd3458f5cdc8b
petyamaiko <petyamaiko@gmail.com>
2025-08-30 15:14:02 +0300

    serve single-page app in production mode

--- server/index.js
+++ server/index.js
@@ -15,11 +15,14 @@ const port = 3001
 if (process.env.NODE_ENV === 'production') {
   ViteExpress.config({ mode: process.env.NODE_ENV })
   app.use(express.static(resolve(__dirname, '../build')))
-  app.use(express.static(resolve(__dirname, '../static')))
 }
 
 app.use('/api', apiRouter)
 bot(app)
+
+app.get('*', (req, res) => {
+  res.sendFile(resolve(__dirname, '../build/index.html'))
+})
 
 ViteExpress.listen(app, port, () => {
   console.log(`App listening on port http://localhost:${port}`)
